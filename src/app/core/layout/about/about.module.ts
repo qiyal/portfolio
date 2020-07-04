2c38ff8fc191226d36ecc8bbb693e2fc9294cb34
Yerbolat <twinspazyl@gmail.com>
2020-07-05 01:17:39 +0600

    add new component Slider in shared package

--- src/app/core/layout/about/about.module.ts
+++ src/app/core/layout/about/about.module.ts
@@ -2,6 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {AboutComponent} from '../../../about/about.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {HttpClientModule} from '@angular/common/http';
+import {CircleDiagramComponent} from '../../shared/circle-diagram/circle-diagram.component';
+import {FormsModule} from '@angular/forms';
+import {SliderComponent} from '../../shared/slider/slider.component';
 
 const routes: Routes = [
   { path: '', component: AboutComponent}
@@ -9,11 +15,17 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [
-    AboutComponent
+    AboutComponent,
+    CircleDiagramComponent,
+    SliderComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    MatButtonModule,
+    MatIconModule,
+    HttpClientModule,
+    FormsModule
   ],
   exports: [
     RouterModule
