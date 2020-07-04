cd40adcd6c9711289181f5f6b2e094dd96210279
Yerbolat <twinspazyl@gmail.com>
2020-07-05 03:00:53 +0600

    add About conponent CSS, HTML and TS code

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {MatIconRegistry} from '@angular/material/icon';
+import {DomSanitizer} from '@angular/platform-browser';
+import {element} from 'protractor';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  dataCardEducation: any[];
 
-  constructor() { }
+  constructor(iconRegistry: MatIconRegistry, sanitized: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'download_custom',
+      sanitized.bypassSecurityTrustResourceUrl('../../assets/icons/about/cloud-computing.svg')
+    );
+  }
 
   ngOnInit(): void {
+
   }
 
 }
