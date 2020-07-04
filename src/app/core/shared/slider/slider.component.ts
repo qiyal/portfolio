a9578fe62b409e93d354810714dac36f39eba79a
Yerbolat <twinspazyl@gmail.com>
2020-07-05 01:29:10 +0600

    add Slider conponent CSS, HTML and TS code

--- src/app/core/shared/slider/slider.component.ts
+++ src/app/core/shared/slider/slider.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slider.component.css']
 })
 export class SliderComponent implements OnInit {
+  noneArr;
+  length: number;
+  index = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.length = 6;
+    this.noneArr = new Array(this.length);
+    console.log("None 0: " + this.noneArr[0]);
+    console.log("Index: " + this.index);
+    console.log("Length: " + this.length);
+  }
+
+  nextCard() {
+    if (this.index < this.length && this.length - this.index - 1 >= 4) {
+      this.noneArr[this.index] = true;
+      this.index++;
+    }
+    console.log("index: " + this.index);
+  }
+
+  prevCard() {
+    if (this.index > 0) {
+      this.index--;
+      this.noneArr[this.index] = false;
+    }
+    console.log("index: " + this.index);
   }
 
 }
