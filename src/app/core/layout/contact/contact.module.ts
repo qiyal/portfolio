c7d1353a4bc317a8c94f6bf0b1caf7ec276867ee
Yerbolat <twinspazyl@gmail.com>
2020-07-05 14:47:36 +0600

    add new component Contact

--- src/app/core/layout/contact/contact.module.ts
+++ src/app/core/layout/contact/contact.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {ContactComponent} from '../../../contact/contact.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ContactComponent
+  ],
   imports: [
     CommonModule
   ]
