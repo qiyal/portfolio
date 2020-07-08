56b388a2a55332434b9a48f1eecc909714a4ca94
Yerbolat <twinspazyl@gmail.com>
2020-07-08 14:51:08 +0600

    add SliderV2 conponent CSS, HTML and TS code

--- src/app/core/layout/projects/projects.module.ts
+++ src/app/core/layout/projects/projects.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ProjectsComponent} from '../../../projects/projects.component';
 import {ProjectsRoutingModule} from './projects-routing/projects-routing.module';
+import {MatIconModule} from '@angular/material/icon';
+import {FooterModule} from '../footer/footer.module';
+import {SliderV2Component} from '../../shared/slider-v2/slider-v2.component';
 
 
 
 @NgModule({
   declarations: [
-    ProjectsComponent
+    ProjectsComponent,
+    SliderV2Component
   ],
-  imports: [
-    CommonModule,
-    ProjectsRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        ProjectsRoutingModule,
+        MatIconModule,
+        FooterModule
+    ]
 })
 export class ProjectsModule { }
--- src/app/core/shared/slider-v2/slider-v2.component.ts
+++ src/app/core/shared/slider-v2/slider-v2.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slider-v2.component.css']
 })
 export class SliderV2Component implements OnInit {
+  length = 9;
+  showCardIndex = 2;
+  page = 2;
+  marginIndex = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  nextShow() {
+    if (this.marginIndex !== 6) {
+      this.marginIndex += 3;
+      this.page += 3;
+      this.showCardIndex = this.page;
+    }
+  }
+
+  prevShow() {
+    if (this.marginIndex !== 0) {
+      this.marginIndex -= 3;
+      this.page -= 3;
+      this.showCardIndex = this.page;
+    }
+  }
+
+  clickCard(index: number) {
+    this.showCardIndex = index;
   }
 
 }
