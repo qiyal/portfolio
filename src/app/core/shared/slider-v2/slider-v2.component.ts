e358efb4f79acf6fc0fe74777e1cfd5e73534453
Yerbolat <twinspazyl@gmail.com>
2020-07-08 14:45:31 +0600

    add new component SliderV2 in shared package

---
+++ src/app/core/shared/slider-v2/slider-v2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slider-v2',
+  templateUrl: './slider-v2.component.html',
+  styleUrls: ['./slider-v2.component.css']
+})
+export class SliderV2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
