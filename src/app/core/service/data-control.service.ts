4bef9231f2e8395c8d444851e3772082f03f5b74
Yerbolat <twinspazyl@gmail.com>
2020-07-10 04:28:03 +0600

    add service DataControlService

---
+++ src/app/core/service/data-control.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataControlService {
+  api = '/user';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  getUserInfo(): Observable<any>{
+    return this.http.get(this.api + '/me');
+  }
+
+  getAllEducation(): Observable<any> {
+    return this.http.get(this.api + '/education');
+  }
+
+  getAllExperience(): Observable<any> {
+    return this.http.get(this.api + '/experience');
+  }
+
+  getAllSkills(): Observable<any> {
+    return this.http.get(this.api + '/skills');
+  }
+}
