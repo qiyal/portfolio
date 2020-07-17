ec6d11eff4a45f607a0ec71f5d6fe559d5181ecc
Yerbolat <twinspazyl@gmail.com>
2020-07-17 20:13:04 +0600

    add new routing 'project/:id' in project-routing.mpdule.ts

--- src/app/core/layout/projects/projects-routing/projects-routing.module.ts
+++ src/app/core/layout/projects/projects-routing/projects-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ProjectsComponent} from '../../../../components/projects/projects.component';
+import {ProjectDetailComponent} from '../../../../components/projects/project-detail/project-detail.component';
 
 const routes: Routes = [
-  { path: '', component: ProjectsComponent }
+  { path: '', component: ProjectsComponent },
+  { path: ':id', component: ProjectDetailComponent}
 ]
 
 @NgModule({
